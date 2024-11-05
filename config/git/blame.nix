24d38179b1333c6338fc7782e5ec58e08f940069
Jakob Ziechmann <48921293+Jakob-Ziechmann@users.noreply.github.com>
2024-11-05 14:31:08 +0100

    added gitblame

---
+++ config/git/blame.nix
@@ -0,0 +1,23 @@
+{
+	plugins.gitblame = {
+		enable = true;
+
+		settings = {
+			enabled = false;
+			date_format = "%r";
+			delay = 0;
+			virtual_text_column = 30;
+			message_template = ''<author> (<date>) "<summary>"'';
+		};
+	};
+
+	keymaps = [
+		{
+			action = "<cmd>GitBlameToggle<CR>";
+				key = "<leader>b";
+				options = {
+					silent = true;
+				};
+		}
+	];
+}
--- config/git/default.nix
+++ config/git/default.nix
@@ -2,5 +2,6 @@
 	imports = [
 		./gitsigns.nix
 		./diffview.nix
+		./blame.nix
 	];
 }
